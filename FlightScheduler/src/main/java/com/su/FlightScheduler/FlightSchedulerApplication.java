650f18e26da6c1aca563d31fccda8dc5af7c9475
Yarkın Siyez <yarkin.siyez@sabanciuniv.edu>
2024-04-24 12:54:06 +0300

    Added Springboot Files

---
+++ FlightScheduler/src/main/java/com/su/FlightScheduler/FlightSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.su.FlightScheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightSchedulerApplication.class, args);
+	}
+
+}
---
+++ FlightScheduler/src/test/java/com/su/FlightScheduler/FlightSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.su.FlightScheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
