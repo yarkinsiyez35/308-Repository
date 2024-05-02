39d20ded9d6e94b390eda593c89af8b53ed661be
sipahibasar <sipahibasar@sabanciuniv.edu>
2024-05-02 16:44:27 +0300

    Added Cabin crew entities
    Added cabin crew entities

---
+++ FlightScheduler/src/main/java/com/su/FlightScheduler/Entity/AttendantLanguagePK.java
@@ -0,0 +1,54 @@
+package com.su.FlightScheduler.Entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+@Embeddable
+public class AttendantLanguagePK implements Serializable {
+    @Column(name = "attendant_id", nullable = false)
+    private int attendantId;
+
+    @Column(name = "language", nullable = false)
+    private String language;
+
+    public AttendantLanguagePK() {
+    }
+
+    public AttendantLanguagePK(int attendantId, String language) {
+        this.attendantId = attendantId;
+        this.language = language;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        AttendantLanguagePK that = (AttendantLanguagePK) o;
+        return attendantId == that.attendantId && Objects.equals(language, that.language);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(attendantId, language);
+    }
+
+    public int getAttendantId() {
+        return attendantId;
+    }
+
+    public void setAttendantId(int attendantId) {
+        this.attendantId = attendantId;
+    }
+
+    public String getLanguage() {
+        return language;
+    }
+
+    public void setLanguage(String language) {
+        this.language = language;
+    }
+}
+
---
+++ FlightScheduler/src/main/java/com/su/FlightScheduler/Entity/CabinCrewEntity.java
@@ -0,0 +1,133 @@
+package com.su.FlightScheduler.Entity;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.util.List;
+
+@Entity
+@Table(name = "crew_members")
+public class CabinCrewEntity implements Serializable {
+    @Id
+    @Column(name = "attendant_id")
+    private int attendantId;
+
+    @Column(name = "email", nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "surname", nullable = false)
+    private String surname;
+
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    @Column(name = "gender", nullable = false)
+    private String gender;
+
+    @Column(name = "nationality", nullable = false)
+    private String nationality;
+
+    @Column(name = "seniority", nullable = false)
+    private String seniority;
+
+    // Constructor
+    @OneToMany(mappedBy = "attendantLanguagePK.attendat_id", cascade = CascadeType.PERSIST)
+    private List<AttendantLanguageEntity> languages;
+    public CabinCrewEntity(int attendantId, String email, String password, String firstName, String surname, int age, String gender, String nationality, String seniority) {
+        this.attendantId = attendantId;
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.surname = surname;
+        this.age = age;
+        this.gender = gender;
+        this.nationality = nationality;
+        this.seniority = seniority;
+    }
+
+    // Getters
+    public int getAttendantId() {
+        return attendantId;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public List<AttendantLanguageEntity> getLanguages() {
+        return languages;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public String getSeniority() {
+        return seniority;
+    }
+
+    // Setters
+    public void setAttendantId(int attendantId) {
+        this.attendantId = attendantId;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public void setSeniority(String seniority) {
+        this.seniority = seniority;
+    }
+    public void setLanguages(List<AttendantLanguageEntity> languages) {
+        this.languages = languages;
+    }
+}
