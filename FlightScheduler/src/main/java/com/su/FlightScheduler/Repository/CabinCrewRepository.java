82fab4bfb26c790bbbd58d7f0139d92e87503aed
Yarkın Siyez <yarkin.siyez@sabanciuniv.edu>
2024-05-03 17:03:02 +0300

    Minor bug fix

--- FlightScheduler/src/main/java/com/su/FlightScheduler/Repository/CabinCrewRepository.java
+++ FlightScheduler/src/main/java/com/su/FlightScheduler/Repository/CabinCrewRepository.java
@@ -2,7 +2,8 @@ package com.su.FlightScheduler.Repository;
 
 import com.su.FlightScheduler.Entity.CabinCrewEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CabinCrewRepository extends JpaRepository<CabinCrewEntity, Integer> {
-
 }
