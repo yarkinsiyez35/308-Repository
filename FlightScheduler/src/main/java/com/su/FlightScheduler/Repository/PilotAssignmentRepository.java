783382cbb3501f918149aaea7d00738b6011c631
Yarkın Siyez <yarkin.siyez@sabanciuniv.edu>
2024-05-07 20:10:57 +0300

    created PilotAssignmentEntity
    created the representation for pilot_languages table and added mapping for PilotEntity. However, not tested with dummy values.

---
+++ FlightScheduler/src/main/java/com/su/FlightScheduler/Repository/PilotAssignmentRepository.java
@@ -0,0 +1,14 @@
+package com.su.FlightScheduler.Repository;
+
+import com.su.FlightScheduler.Entity.PilotAssignmentEntity;
+import com.su.FlightScheduler.Entity.PilotAssignmentPK;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface PilotAssignmentRepository extends JpaRepository<PilotAssignmentEntity, PilotAssignmentPK> {
+    public List<PilotAssignmentEntity> findAllByPilotAssignmentPK_PilotId(int pilotId);
+    public List<PilotAssignmentEntity> findAllByPilotAssignmentPK_FlightNumber(String flightNumber);
+}
