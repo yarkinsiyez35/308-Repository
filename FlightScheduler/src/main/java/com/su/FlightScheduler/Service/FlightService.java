8e18076b78a0d0bb33df06a3b2955fa2ed5ea549
Fevzi Kağan Becel <kaganbecel@gmail.com>
2024-05-22 21:21:31 +0300

    Updates and modifications required to satisfy issues #53, #54, and #55

--- FlightScheduler/src/main/java/com/su/FlightScheduler/Service/FlightService.java
+++ FlightScheduler/src/main/java/com/su/FlightScheduler/Service/FlightService.java
@@ -1,29 +1,63 @@
 package com.su.FlightScheduler.Service;
 
 import com.su.FlightScheduler.Entity.*;
+import com.su.FlightScheduler.Repository.VehicleTypeRepository;
 
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.Map;
 
 public interface FlightService {
-    FlightEntity saveFlight(FlightEntity flight, AdminEntity admin);
-    FlightEntity createFlight(String flightNumber, String flightInfo, AirportEntity sourceAirport, AirportEntity destinationAirport, PlaneEntity plane, Integer flightRange, LocalDateTime departureDateTime, LocalDateTime landingDateTime, boolean sharedFlight, CompanyEntity sharedFlightCompany, AdminEntity admin, String standardMenu);
-    FlightEntity createFlight(String flightNumber, String flightInfo, AirportEntity sourceAirport, AirportEntity destinationAirport, AdminEntity admin);
+    FlightEntity saveFlightObj(FlightEntity flight);
+    FlightEntity createFlightFilled(String flightNumber, String flightInfo, AirportEntity
+            sourceAirport, AirportEntity destinationAirport, PlaneEntity plane, Integer flightRange,
+                                    LocalDateTime departureDateTime,
+                                    LocalDateTime landingDateTime, boolean sharedFlight,
+                                    CompanyEntity sharedFlightCompany, AdminEntity admin,
+                                    String standardMenu);
     FlightEntity createFlight(String flightNumber, String flightInfo, AdminEntity admin);
-    Optional<FlightEntity> findFlightByNumber(String flightNumber, AdminEntity admin);
-    List<FlightEntity> findAllFlights(AdminEntity admin);
-    FlightEntity updateFlight(FlightEntity flight, AdminEntity admin);
-    void deleteFlightByNumber(String flightNumber, AdminEntity admin);
-    FlightEntity updateFlightInfo(String flightNumber, String flightInfo, AdminEntity admin);
-    FlightEntity updateSourceAirport(String flightNumber, AirportEntity sourceAirport, AdminEntity admin);
-    FlightEntity updateDestinationAirport(String flightNumber, AirportEntity destinationAirport, AdminEntity admin);
-    FlightEntity updatePlane(String flightNumber, PlaneEntity plane, AdminEntity admin);
-    FlightEntity updateFlightRange(String flightNumber, Integer flightRange, AdminEntity admin);
-    FlightEntity updateDepartureDateTime(String flightNumber, LocalDateTime departureDateTime, AdminEntity admin);
-    FlightEntity updateLandingDateTime(String flightNumber, LocalDateTime landingDateTime, AdminEntity admin);
-    FlightEntity updateSharedFlight(String flightNumber, boolean sharedFlight, AdminEntity admin);
-    FlightEntity updateSharedFlightCompany(String flightNumber, CompanyEntity sharedFlightCompany, AdminEntity admin);
-    /*FlightEntity updateAdmin(String flightNumber, AdminEntity admin);*/
-    FlightEntity updateStandardMenu(String flightNumber, String standardMenu, AdminEntity admin);
+    FlightEntity addFlightParams1(String flightNumber, PlaneEntity plane, AirportEntity sourceAirport, AirportEntity destinationAirport);
+    FlightEntity addFlightParams2(String flightNumber, Integer flightRange, LocalDateTime departureDateTime, LocalDateTime landingDateTime);
+    FlightEntity addFlightParams3(String flightNumber, boolean sharedFlight, CompanyEntity sharedFlightCompany);
+
+    Optional<FlightEntity> findFlightByNumber(String flightNumber);
+    List<FlightEntity> findAllFlights();
+    FlightEntity updateFlight(FlightEntity flight);
+    void deleteFlightByNumber(String flightNumber);
+
+    // Update methods for the attributes of the FlightEntity
+    FlightEntity updateFlightInfo(String flightNumber, String flightInfo);
+    FlightEntity updateSourceAirport(String flightNumber, AirportEntity sourceAirport);
+    FlightEntity updateDestinationAirport(String flightNumber, AirportEntity destinationAirport);
+    FlightEntity updatePlane(String flightNumber, PlaneEntity plane);
+    FlightEntity updateFlightRange(String flightNumber, Integer flightRange);
+    FlightEntity updateDepartureDateTime(String flightNumber, LocalDateTime departureDateTime);
+    FlightEntity updateLandingDateTime(String flightNumber, LocalDateTime landingDateTime);
+    FlightEntity updateSharedFlight(String flightNumber, boolean sharedFlight);
+    FlightEntity updateSharedFlightCompany(String flightNumber, CompanyEntity sharedFlightCompany);
+    FlightEntity updateStandardMenu(String flightNumber, String standardMenu);
+    // End of update methods for the attributes of the FlightEntity
+
+
+    // Getters for the entities
+    AirportEntity getSourceAirport(String flightNumber);
+    AirportEntity getDestinationAirport(String flightNumber);
+    PlaneEntity getPlane(String flightNumber);
+    CompanyEntity getCompany(String flightNumber);
+    LocalDateTime getDateTime(String flightNumber);
+    // End of getters for the entities
+
+
+    // Getters for the entities
+    AirportEntity getAirportFromRequest(Map<String, Object> request, String key);
+    PlaneEntity getPlaneFromRequest(Map<String, Object> request, String key);
+    CompanyEntity getCompanyFromRequest(Map<String, Object> request, String key);
+    LocalDateTime getDateTimeFromRequest(Map<String, Object> request, String key);
+
+    // Getters for DTOs (Projections)
+    VehicleTypeRepository.SeatingPlanProjection findSeatingPlanByFlightNumber(String flightNumber);
+    VehicleTypeRepository.AttendeeCapacityProjection findAttendeeCapacityByFlightNumber(String flightNumber);
+    VehicleTypeRepository.PilotCapacityProjection findPilotCapacityByFlightNumber(String flightNumber);
+    // End of getters for DTOs (Projections)
 }
